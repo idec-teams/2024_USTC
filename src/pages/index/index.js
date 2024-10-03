e228a28d74cf7d7a32e23b1fbf7c907c8a5b32ab
zichongxu-ustc <zchong_xu@mail.ustc.edu.cn>
2024-10-03 23:21:12 +0800

    Regular upload

--- src/pages/index/index.js
+++
@@ -1,6 +0,0 @@
-
-import "@src/pages/index/index.css";
-import textboxcreate from "@src/textbox/textbox.js";
-import indextext from "@pages/index/indextext.json";
-
-textboxcreate(indextext);
--- src/pages/member/member.js
+++
@@ -1,1 +0,0 @@
-import "@src/memberbox/memberbox.js/"
